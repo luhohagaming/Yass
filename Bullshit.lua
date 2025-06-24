8fd9c2d85d0384142a14d06d02139baf822e6706
Luhoha <74718213+luhohagaming@users.noreply.github.com>
2025-06-25 00:40:52 +0200

    Fix unconditional printing in Lua script

--- Bullshit.lua
+++ Bullshit.lua
@@ -1,5 +1,8 @@
-if "bullshit"
-  then
-  print("bullshit")
+-- Print "bullshit" only when the first command line argument is the
+-- string "bullshit".  This avoids printing unconditionally.
+
+if arg and arg[1] == "bullshit" then
+    print("bullshit")
 else
+    print("no bullshit")
 end
